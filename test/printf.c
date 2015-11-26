68a8e599a8d31a9c33ce51ed222721350a25c3fb
Hans Anderson <hans@hanscanderson.com>
2015-11-26 12:14:57 -0600

    Moved newlines into TPRINTF macro for printf tests
    This makes the printf.c and sprintf.c files easier to diff.

--- test/printf.c
+++ test/printf.c
@@ -13,8 +13,8 @@
 #define UNUSED(x) (void)(x)
 
 #define TPRINTF(expr...) \
-  ({ printf("libc_printf(%s) -> ", #expr); printf(expr); \
-     printf(" tfp_printf(%s) -> ", #expr); tfp_printf(expr); })
+  ({ printf("libc_printf(%s) -> ", #expr); printf(expr); printf("\n"); \
+     printf(" tfp_printf(%s) -> ", #expr); tfp_printf(expr); printf("\n"); })
 
 static void stdout_putf(void *unused, char c)
 {
@@ -28,41 +28,41 @@ int main()
 
   printf("Fun with printf and %%!\n");
 
-  TPRINTF("d1=%016llx d2=%016lx d3=%02x d4=%02X 42=%03d\n",
+  TPRINTF("d1=%016llx d2=%016lx d3=%02x d4=%02X 42=%03d",
           (long long unsigned)0xd1, (long unsigned)0xd2, 0xd3, 0xd4, 42);
-  TPRINTF("d1=%04x d2=%06x d3=%08x %%100\n", 0xd1, 0xd2, 0xd3);
-  TPRINTF("|%-14s| |%-16s| d2=%2x |%-30s|\n", "str14", "str16", 0xd2,
+  TPRINTF("d1=%04x d2=%06x d3=%08x %%100", 0xd1, 0xd2, 0xd3);
+  TPRINTF("|%-14s| |%-16s| d2=%2x |%-30s|", "str14", "str16", 0xd2,
           "12345678901234567890123456789012345");
-  TPRINTF("|%4s|\n", "string4");
-  TPRINTF("|%-4s|\n", "string4");
-  TPRINTF("42=%3d d1=%4.4x |%4s| d2=%8.8x\n", 42, 0xd1, "string4", 0xd2);
-  TPRINTF("42=%3d d1=%4.4x |%-4s| d2=%8.8x\n", 42, 0xd1, "string4", 0xd2);
-  TPRINTF("84=%d 21=%ds |%s| |%sOK| d1=%x d2=%#x\n",
+  TPRINTF("|%4s|", "string4");
+  TPRINTF("|%-4s|", "string4");
+  TPRINTF("42=%3d d1=%4.4x |%4s| d2=%8.8x", 42, 0xd1, "string4", 0xd2);
+  TPRINTF("42=%3d d1=%4.4x |%-4s| d2=%8.8x", 42, 0xd1, "string4", 0xd2);
+  TPRINTF("84=%d 21=%ds |%s| |%sOK| d1=%x d2=%#x",
           84, 21, "hello", "fine", 0xd1, 0xd2);
 
-  TPRINTF("%lld\n", LLONG_MIN);
-  TPRINTF("%lld\n", LLONG_MAX);
-  TPRINTF("%llu\n", ULLONG_MAX);
-  TPRINTF("%llx\n", LLONG_MIN);
-  TPRINTF("%llx\n", LLONG_MAX);
-  TPRINTF("%llx\n", ULLONG_MAX);
+  TPRINTF("%lld", LLONG_MIN);
+  TPRINTF("%lld", LLONG_MAX);
+  TPRINTF("%llu", ULLONG_MAX);
+  TPRINTF("%llx", LLONG_MIN);
+  TPRINTF("%llx", LLONG_MAX);
+  TPRINTF("%llx", ULLONG_MAX);
 
-  TPRINTF("d1=%.1x\n", 0xd1);
-  TPRINTF("d1=%4.1x\n", 0xd1);
-  TPRINTF("d1=%4.x\n", 0xd1);
+  TPRINTF("d1=%.1x", 0xd1);
+  TPRINTF("d1=%4.1x", 0xd1);
+  TPRINTF("d1=%4.x", 0xd1);
 
   {
     char blah[256];
-    TPRINTF("a=%zd\n", sizeof(blah));
-    TPRINTF("a=%zu\n", sizeof(blah));
-    TPRINTF("a=%zi\n", sizeof(blah));
-    TPRINTF("a=0x%zx\n", sizeof(blah));
+    TPRINTF("a=%zd", sizeof(blah));
+    TPRINTF("a=%zu", sizeof(blah));
+    TPRINTF("a=%zi", sizeof(blah));
+    TPRINTF("a=0x%zx", sizeof(blah));
   }
 
   {
     int in_stack;
-    TPRINTF("Adddress of main: %p\n", main);
-    TPRINTF("Adddress of stack variable: %p\n", &in_stack);
+    TPRINTF("Adddress of main: %p", main);
+    TPRINTF("Adddress of stack variable: %p", &in_stack);
   }
 
   return 0;
